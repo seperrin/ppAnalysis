42b778644ecfcc708f1c6faa0ead7fff78d48eec
Sebastien Perrin <sebastien.perrin@cern.ch>
2020-12-21 11:26:35 +0100

    Added study of Percentile Method

--- PlotFromTreeCINT.C
+++ PlotFromTreeCINT.C
@@ -21,6 +21,7 @@
 # include "TProfile.h"
 # include "TVirtualFitter.h"
 # include "TFitter.h"
+# include "TProfile.h"
 # include "TMinuit.h"
 # include "TRandom.h"
 # include <iostream>
@@ -72,14 +73,22 @@ void PlotFromTreeCINT(){
     const int NbinsDeltaEta = 20;
     double SizeBinDeltaEta = (MaxDeltaEta-MinDeltaEta)/NbinsDeltaEta;
     int barWidth = 50;
+    
+    const double binsCent[6] = {0,1,10,20,40,100};
 // *************************
 // Initialiser les graphes *
 // *************************
     
     TH1F* hnseg(NULL);
+    TH1* PMSliced[20]={NULL};
+    TH2F* PercentileMethod(NULL);
+    TH2F* PercentileMethodLimits(NULL);
     TH1F* hnseg_raw(NULL);
     TH1F* hnseg_den(NULL);
     TH1F* hnseg_spdtkl(NULL);
+    TH1F* hnseg_spdtklval(NULL);
+    TH1F* hnseg_spdtklval_dist(NULL);
+    TProfile* hn(NULL);
     
     
     hnseg = new TH1F("hnseg",
@@ -88,7 +97,19 @@ void PlotFromTreeCINT(){
     hnseg->SetXTitle("Zvtx");
     hnseg->SetYTitle("Mean Ntkl");
     
-    hnseg_raw = new TH1F("hnseg",
+    PercentileMethod = new TH2F("PercentileMethod",
+                     "Ntkl wrt Zvtx",
+                     20,-10,10,100,0,100);
+    PercentileMethod->SetXTitle("Zvtx");
+    PercentileMethod->SetYTitle("Ntkl");
+    
+    PercentileMethodLimits = new TH2F("PercentileMethodLimits",
+                     "Ntkl Limit wrt Zvtx and percentile",
+                                      20,-10,10,5,binsCent);
+    PercentileMethodLimits->SetXTitle("Zvtx");
+    PercentileMethodLimits->SetYTitle("Percentile");
+    
+    hnseg_raw = new TH1F("hnseg_raw",
                      "Mean raw Ntkl wrt Zvtx",
                      21,-10,10);
     hnseg_raw->SetXTitle("Zvtx");
@@ -105,14 +126,32 @@ void PlotFromTreeCINT(){
                      21,-10,10);
     hnseg_spdtkl->SetXTitle("Zvtx");
     hnseg_spdtkl->SetYTitle("Mean SPDTkl");
+    
+    hnseg_spdtklval = new TH1F("hnseg_spdtklval",
+                        "Mean SPDTklVal wrt Zvtx",
+                        21,-10,10);
+       hnseg_spdtklval->SetXTitle("Zvtx");
+       hnseg_spdtklval->SetYTitle("Mean SPDTklVal");
+    
+    hnseg_spdtklval_dist = new TH1F("hnseg_spdtklval_dist",
+                     "Distribution of SPDTkl value",
+                     160,0,160);
+    hnseg_spdtklval_dist->SetXTitle("SPDTkl value");
+    hnseg_spdtklval_dist->SetYTitle("Count");
+    
+    hn = new TProfile("hn",
+                     "Mean SPDTklVal wrt Zvtx TPro",
+                     21,-10,10);
+    hn->SetXTitle("Zvtx");
+    hn->SetYTitle("Mean SPDTklVal");
 
 // *************************
 // Analyse                 *
 // *************************
     
     
-        
-    TFile fileIn("~/../../Volumes/Transcend2/ppAnalysis/Scripts/GoodLHC16h_pass1_MinBias/muonGrid.root");
+    // TFile fileIn("~/../../Volumes/Transcend2/ppAnalysis/Scripts/muonGrid.root");
+    TFile fileIn("~/../../Volumes/Transcend2/ppAnalysis/Scripts/CINT_16o_PS_CutsEvent_Val_CENT/muonGrid.root");
     std::cout << "1" <<std::endl;
         TTree* theTree = NULL;
         fileIn.GetObject("MyMuonTree",theTree);
@@ -177,20 +216,65 @@ void PlotFromTreeCINT(){
             for (Int_t j=0; j<fEvent->fNTracklets; j++) {
                 trac = (TrackletLight*)fTracklets->At(j);
                 if((TMath::Abs(trac->fEta) < 1)){
-                    if((TMath::Abs(trac->fDPhi) < DPhiCut)){
                         NumberCloseEtaTracklets++;
-                    }
+                    
                 }
                 
             }
-            hnseg_spdtkl->Fill(fEvent->fVertexZ, fEvent->fCentralitySPDTracklets);
             
-             hnseg->Fill(fEvent->fVertexZ, NumberCloseEtaTracklets);
-             hnseg_raw->Fill(fEvent->fVertexZ, fEvent->fNTracklets);
-             hnseg_den->Fill(fEvent->fVertexZ);
+            if(fEvent->fVertexNC >= 1 && fEvent->fNPileupVtx == 0){
+                hnseg_spdtkl->Fill(fEvent->fVertexZ, fEvent->fCentralitySPDTracklets);
+               hnseg_spdtklval->Fill(fEvent->fVertexZ, fEvent->fSPDTrackletsValue);
+                hn->Fill(fEvent->fVertexZ, fEvent->fSPDTrackletsValue);
+                hnseg_den->Fill(fEvent->fVertexZ);
+                hnseg_raw->Fill(fEvent->fVertexZ, fEvent->fNTracklets);
+                hnseg->Fill(fEvent->fVertexZ, NumberCloseEtaTracklets);
+                PercentileMethod->Fill(fEvent->fVertexZ,NumberCloseEtaTracklets);
+               }
+            hnseg_spdtklval_dist->Fill(fEvent->fSPDTrackletsValue);
+            
+            // hnseg->Fill(fEvent->fVertexZ, NumberCloseEtaTracklets);
+           //  hnseg_raw->Fill(fEvent->fVertexZ, fEvent->fNTracklets);
             
             
         }
+    
+    TCanvas*cPMproj=new TCanvas();
+    cPMproj->Divide(4,5);
+    for(int i=0;i<20;i++){
+        cPMproj->cd(i);
+        PMSliced[i] = PercentileMethod->ProjectionY(Form("bin%d_0",i+1),i+1,i+1);
+        PMSliced[i]->Draw("e");
+    }
+    
+    cout << "PMSliced[0] info: " << PMSliced[0]->GetEntries() <<endl;
+    
+    for(int i=0; i<20; i++){
+        int nentries = PMSliced[i]->GetEntries();
+        int sum = 0;
+        int j = 1;
+        while(sum<nentries){
+            sum += PMSliced[i]->GetBinContent(j);
+            double ratio = double(sum)/nentries;
+            if(i==0){
+                cout << "i: "<< i<<" j: " << j << " sum: " << sum << " ratio: " << ratio <<endl;
+            }
+            if(ratio<0.99){
+                PercentileMethodLimits->Fill((i-10)+0.5, 0.5);
+                if(ratio<0.90){
+                    PercentileMethodLimits->Fill((i-10)+0.5, 5.5);
+                    if(ratio<0.80){
+                        PercentileMethodLimits->Fill((i-10)+0.5, 15);
+                        if(ratio<0.60){
+                            PercentileMethodLimits->Fill((i-10)+0.5, 30);
+                        }
+                    }
+                }
+            }
+            j++;
+        }
+    }
+    
     cout << "Reading of events finished" <<endl;
 
     std::cout << "==================== Analysis Terminated ====================" << std::endl;
@@ -225,6 +309,28 @@ void PlotFromTreeCINT(){
     hnseg_spdtkl->Divide(hnseg_den);
     hnseg_spdtkl->SetFillColor(18);
     hnseg_spdtkl->Draw();
+    
+    TCanvas*c4=new TCanvas();
+    hnseg_spdtklval->Divide(hnseg_den);
+    hnseg_spdtklval->SetFillColor(18);
+    hnseg_spdtklval->Draw();
+    
+    TCanvas*c4Pro=new TCanvas();
+    hn->SetFillColor(18);
+    hn->Draw();
+    
+    TCanvas*c45=new TCanvas();
+    hnseg_spdtklval_dist->SetFillColor(18);
+    hnseg_spdtklval_dist->Draw("e");
+    
+    TCanvas*cPM=new TCanvas();
+    PercentileMethod->SetFillColor(18);
+    PercentileMethod->Draw("colz");
+    
+    TCanvas*cPMLim=new TCanvas();
+    PercentileMethodLimits->SetFillColor(18);
+    PercentileMethodLimits->Draw("colz");
+    
     
 }
     
